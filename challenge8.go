1df2662d0aa83569f8c8ef8ccab2798d8e06081f
Keller <david.keller@RMD-MAC-109.local>
2019-10-28 15:50:45 -0600

    Refactor ProductChecker

--- challenge8.go
+++ challenge8.go
@@ -20,28 +20,20 @@ func HandleInt() []int64 {
 	return digits
 }
 
-// func ReducePrimes(allPrimes []int) int {
-// 	sum := 0
-
-// 	for _, v := range allPrimes {
-// 		sum += v
-// 	}
-// 	return sum
-// }
-
 func ProductChecker(digits []int64) int64 {
-	// var TempProduct int64 = 0
+	var TempProduct int64 = 1
 	var FinalProduct int64 = 0
 
 	for i := 0; i < 13; i++ {
-		for _, v := range digits {
-			FinalProduct *= v
+		TempProduct = TempProduct * digits[i]
+		if TempProduct > FinalProduct {
+			FinalProduct = TempProduct
+			fmt.Println(FinalProduct)
 		}
 	}
 	return FinalProduct
 }
 
 func main() {
-	fmt.Println(ProductChecker(HandleInt()))
-	// fmt.Println(HandleInt())
+	ProductChecker(HandleInt())
 }
